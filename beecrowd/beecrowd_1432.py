0e3a14ab39bd9c0da46954ddeb5756bde02c90b2
eliasroodrigues <elias.roodrigues98@gmail.com>
2022-07-13 17:37:13 -0300

    1432

--- beecrowd/beecrowd_1432.py
+++ beecrowd/beecrowd_1432.py
@@ -1,63 +1,69 @@
 """
-  Triple-Free Binary Strings
+  Triple-Free Binary Strings #1432
 
   Link: https://www.beecrowd.com.br/judge/pt/problems/view/1432
 
   Author: Elias E. S. Rodrigues
 """
 
-memo = {}
-
 def check_triple_free(pattern, pos):
-  z = u = a = r = 0
+  limit = int(pos / 3)
 
-  for i in pattern:
-    if i == '0':
-      z += 1    
-    elif i == '1':
-      u += 1
-    else:
-      a += 1
-      z += 1
-      u += 1
+  for i in range(1, limit+1):
+    pos3 = pos - i
+    pos2 = pos3 - i
+    pos1 = pos2 - i
+    flag = True
 
-  if z == 3 or u == 3:
-    r += 1
-  
-  if a == 3:
-    r += 1
-  
-  return r
+    for j in range(i):      
+      if pattern[pos1+j] != pattern[pos2+j] or pattern[pos2+j] != pattern[pos3+j]:
+        flag = False
+        break
 
-def binary_string(pattern, pos, result):
-  print(f'Pattern: {pattern}, pos: {pos}, result: {result}')
+    if flag:
+      return False
 
-  if len(pattern) >= 3:
-    key = ''.join(pattern[:3])
-    print(f'Key: {key}')
-    if key in memo:
-      result += memo[key]
-    else:
-      memo[key] = check_triple_free(pattern[:3], 0)
-      result += memo[key]
-    pos += 1
-    return binary_string(pattern[pos:], pos, result)
+  return True
+
+def tf_binary_string(pattern, n, pos):
+  """
+    If the position that will be checked is equal to the string
+    length, return 1.
+  """ 
+  if pos == n:
+    return 1
+
+  result = 0
+
+  if pattern[pos] == '*':
+    pattern[pos] = '0'
+    if check_triple_free(pattern, pos+1):
+      result += tf_binary_string(pattern, n, pos+1)
+    pattern[pos] = '1'
+    if check_triple_free(pattern, pos+1):
+      result += tf_binary_string(pattern, n, pos+1)
+    pattern[pos] = '*'
   else:
-    return result
+    if check_triple_free(pattern, pos+1):
+      result += tf_binary_string(pattern, n, pos+1)
+
+  return result
 
 if __name__ == '__main__':
-  i = 1
+  i = 0
 
   while True:
     op = input().split()
+
+    # op = cases[i].split()
 
     if op[0] == '0':
       break
 
     pattern = [num for num in op[1]]
 
-    print(f'Case {i}: {binary_string(pattern, 0, 0)}')
+    answer = tf_binary_string(pattern, len(pattern), 0)
 
-    print(memo)
+    print(f'Case {i}: {answer}')
 
     i += 1
